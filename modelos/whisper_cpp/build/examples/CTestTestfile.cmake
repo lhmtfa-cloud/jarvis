3b3111364dd67cef35e8d8e7ec0963b546ececb4
RafaelAEG <rafael.evangelista.goncalves@gmail.com>
2025-10-19 14:21:53 -0300

    base2

---
+++ modelos/whisper_cpp/build/examples/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/rafae/Documents/GitHub/jarvis/whisper_cpp/examples
+# Build directory: C:/Users/rafae/Documents/GitHub/jarvis/whisper_cpp/build/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cli")
+subdirs("bench")
+subdirs("server")
+subdirs("quantize")
+subdirs("vad-speech-segments")
+subdirs("deprecation-warning")
